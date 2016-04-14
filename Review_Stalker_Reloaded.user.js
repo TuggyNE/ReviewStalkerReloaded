6e389c095bf2df49531d1bdd0d09b4c63962aa06
Nathan Tuggy <nathan@tuggycomputer.com>
2016-04-14 16:39:01 -0500

    Refactored to improve modularity. No release.

--- Review_Stalker_Reloaded.user.js
+++ Review_Stalker_Reloaded.user.js
@@ -9,7 +9,7 @@
 // @include     *://*.askubuntu.com/review*
 // @include     *://*.mathoverflow.net/review*
 // @include     *://*.stackapps.net/review*
-// @version     1.4.07
+// @version     1.5.03
 // @grant       GM_openInTab
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -37,11 +37,10 @@ var DomMain = location.hostname, BDomInL = LDomSites.indexOf(DomMain) > -1;
 var BMotherMeta = BIsMotherMeta(DomMain);
 var BChildMeta = !BMotherMeta && DomMain.startsWith("meta.");
 if (BChildMeta) { DomMain = DomMain.substring("meta.".length); }
-var ElemHeader = document.querySelector(".subheader.tools-rev"), ElemMetaLoadProgress;
-var LHrefToOpen = [];
 
-var BRecycleTab = history.length >= NTotalNavRecycleTab && NTotalNavRecycleTab != -1;
+
 function CheckNextPage() {
+  var BRecycleTab = history.length >= NTotalNavRecycleTab && NTotalNavRecycleTab != -1;
   var i = LDomSites.indexOf(DomMain) + 1, DomNext = LDomSites[i % LDomSites.length];
   if (i >= LDomSites.length) {
     NNavLoad = (NNavLoad % NNavLoadMeta) + 1;
@@ -62,17 +61,6 @@ function CheckNextPage() {
   }
 }
 
-function BCapped(ElemInstr) {
-  return ElemInstr && ElemInstr.textContent.startsWith("Thank you for reviewing ");
-}
-function BEmpty(ElemInstr) {
-  return ElemInstr && ElemInstr.textContent.startsWith("This queue has been cleared!");
-}
-function BLoading() {
-  return !!document.querySelector(".review-actions .ajax-loader");
-}
-var BPaused = false, DtFirstLoaded = new Date();
-
 var HrefOriginalFavicon;
 function SetFavicon(HrefIcon) {
   var NlLinkIcon = document.querySelectorAll("link[rel*='icon']");
@@ -87,99 +75,152 @@ function SetFavicon(HrefIcon) {
   ElemLinkIcon.href = HrefIcon;
   document.head.appendChild(ElemLinkIcon);
 }
-var TitleBase = document.title;
-if (BInQueue) {
-  TitleBase = document.title.replace(/^Review /, "");
-  var TmrFaviconTitleMarker = setInterval(function() {
-    let status = document.querySelector("div.review-status");
-    if (status) {
-      document.title = TitleBase;
-      clearInterval(TmrFaviconTitleMarker);
+
+function BCapped(ElemInstr) {
+  return ElemInstr && ElemInstr.textContent.startsWith("Thank you for reviewing ");
+}
+function BEmpty(ElemInstr) {
+  return ElemInstr && ElemInstr.textContent.startsWith("This queue has been cleared!");
+}
+function BLoading() {
+  return !!document.querySelector(".review-actions .ajax-loader");
+}
+
+var BPaused = false, TitleBase = document.title, TmrQueueStatus, DtFirstLoaded = new Date();
+function CheckQueueStatus() {
+  let status = document.querySelector("div.review-status");
+  if (status) {
+    document.title = TitleBase;
+    clearInterval(TmrQueueStatus);
+    BPaused = true;
+  }
+  else {
+    let instr = document.querySelector("span.review-instructions.infobox");
+    if (BCapped(instr)) {
+      setTimeout(function () { CheckNextPage(); }, 5 * 1000);
+      clearInterval(TmrQueueStatus);
+    }
+    else if (BLoading()) {
+      document.title = "… " + TitleBase;
+      BPaused = true;
+    }
+    else if (BEmpty(instr)) {
+      document.title = "∅ " + TitleBase;
+      SetFavicon(HrefBlankFavicon);
+      BPaused = false;
+      clearInterval(TmrQueueStatus);
+      //console.log("Unpausing because " + document.location.href + " is empty.");
+    }
+    else if ((new Date()).valueOf() - DtFirstLoaded.valueOf() < MsiReloadStale) {
+      document.title = "🔎 " + TitleBase;
+      SetFavicon(HrefOriginalFavicon);
       BPaused = true;
     }
     else {
-      let instr = document.querySelector("span.review-instructions.infobox");
-      if (BCapped(instr)) {
-        setTimeout(function () { CheckNextPage(); }, 5 * 1000);
-        clearInterval(TmrFaviconTitleMarker);
-      }
-      else if (BLoading()) {
-        document.title = "… " + TitleBase;
-        BPaused = true;
-      }
-      else if (BEmpty(instr)) {
-        document.title = "∅ " + TitleBase;
-        SetFavicon(HrefBlankFavicon);
-        BPaused = false;
-        clearInterval(TmrFaviconTitleMarker);
-        //console.log("Unpausing because " + document.location.href + " is empty.");
-      }
-      else if ((new Date()).valueOf() - DtFirstLoaded.valueOf() < MsiReloadStale) {
-        document.title = "🔎 " + TitleBase;
-        SetFavicon(HrefOriginalFavicon);
-        BPaused = true;
-      }
-      else {
-        // Let an aged-out review item go; if it comes back, that's fine
-        BPaused = false;
-      }
+      // Let an aged-out review item go; if it comes back, that's fine
+      BPaused = false;
     }
-  }, 0.25 * 1000);
+  }
 }
-else {
-  SetFavicon(GM_getResourceURL("icon"));
 
-  var NlNumAvailable = document.querySelectorAll(".dashboard-count:not(.dashboard-faded) > .dashboard-num");
+var NlNumAvailable = document.querySelectorAll(".dashboard-count:not(.dashboard-faded) > .dashboard-num");
+function GetLHrefToOpen() {
+  var LHref = [];
   for (let NNumAvailable of NlNumAvailable) {
     let SNumAvailable = NNumAvailable.title;
     //console.log(SNumAvailable);
     if (Number.parseInt(SNumAvailable) > 0) {
       let NLnkAvailable = NNumAvailable.parentNode.parentNode.querySelector(".dashboard-title > a");
       //console.log("adding " + NLnkAvailable.href);
-      LHrefToOpen.push(NLnkAvailable.href);
+      LHref.push(NLnkAvailable.href);
     }
   }
+  return LHref;
+}
+
+function CreateElemMetaLoadProgress() {
+  var ElemMetaLoadProgress = document.createElement("span");
   
-  if (!BChildMeta) {
-    ElemMetaLoadProgress = document.createElement("span");
-    ElemMetaLoadProgress.style.cssFloat = "right";
-    ElemMetaLoadProgress.style.marginTop = "1em";
-    ElemMetaLoadProgress.style.fontSize = "0.85em";
-    ElemMetaLoadProgress.textContent = GM_info.script.name + " v" + GM_info.script.version;
-    ElemMetaLoadProgress.textContent += "; meta load: " + NNavLoad + "/" + NNavLoadMeta;
-    if (NTotalNavRecycleTab != -1) ElemMetaLoadProgress.textContent += "; tab recycle: " + history.length + "/" + NTotalNavRecycleTab;
-    ElemHeader.appendChild(ElemMetaLoadProgress);
-    
-    if (!BDomInL && (NlNumAvailable.length > 0)) {
-      // If there are any enabled queues on a main site, this user has review privileges here
-      ElemMetaLoadProgress.textContent += " — site added!";
-      LDomSites.push(DomMain);
-      let SLDomSitesNew = LDomSites.join(",").replace(/^,|,,|,$/, '');
-      //console.log("Site list: '" + GM_getValue("LDomSites", "") + "' -> '" + SLDomSitesNew + "'");
-      GM_setValue("LDomSites", SLDomSitesNew);
-    }
-    else if (BDomInL && (NlNumAvailable.length == 0)) {
-      ElemMetaLoadProgress.textContent += " — site removed!";
-      let i = LDomSites.indexOf(DomMain);
-      if (i > -1) {
-        LDomSites.splice(i, 1);
-        let SLDomSitesNew = LDomSites.join(",").replace(/^,|,,|,$/, '');
-        //console.log("Site list: '" + GM_getValue("LDomSites", "") + "' -> '" + SLDomSitesNew + "'");
-        GM_setValue("LDomSites", SLDomSitesNew);
-      }
-      else {
-        console.log("Unable to find site " + DomMain + " to remove it in '" + LDomSites.join(",") + "'!");
-      }
-    }
-    else if (!BDomInL) {
-      // No review access, evidently, so clear things out just to be sure and don't display any header
-      //console.log("Site list: '" + LDomSites + "'");
-      LHrefToOpen = [];
-      ElemHeader.removeChild(ElemMetaLoadProgress);
-    }
+  ElemMetaLoadProgress.style.cssFloat = "right";
+  ElemMetaLoadProgress.style.marginTop = "1em";
+  ElemMetaLoadProgress.style.fontSize = "0.85em";
+  ElemMetaLoadProgress.textContent = GM_info.script.name + " v" + GM_info.script.version;
+  ElemMetaLoadProgress.textContent += "; meta load: " + NNavLoad + "/" + NNavLoadMeta;
+  if (NTotalNavRecycleTab != -1) ElemMetaLoadProgress.textContent += "; tab recycle: " + history.length + "/" + NTotalNavRecycleTab;
+  
+  return ElemMetaLoadProgress;
+}
+
+function AddSite(Dom) {
+  if (BDomInL) return false;
+  LDomSites.push(Dom);
+  let SLDomSitesNew = LDomSites.join(",").replace(/^,|,,|,$/, '');
+  //console.log("Site list: '" + GM_getValue("LDomSites", "") + "' -> '" + SLDomSitesNew + "'");
+  GM_setValue("LDomSites", SLDomSitesNew);
+  return true;
+}
+function RemoveSite(Dom) {
+  if (!BDomInL) return false;
+  let i = LDomSites.indexOf(DomMain);
+  if (i > -1) {
+    LDomSites.splice(i, 1);
+    let SLDomSitesNew = LDomSites.join(",").replace(/^,|,,|,$/, '');
+    //console.log("Site list: '" + GM_getValue("LDomSites", "") + "' -> '" + SLDomSitesNew + "'");
+    GM_setValue("LDomSites", SLDomSitesNew);
+    return true;
+  }
+  else {
+    console.log("Unable to find site " + DomMain + " to remove it in '" + LDomSites.join(",") + "'!");
+    return false;
+  }
+}
+function CheckSiteMembership(NQueueAvailable, ElemContainer, ElemStatus) {
+  if (NQueueAvailable > 0 && AddSite(DomMain, ElemContainer)) {
+    ElemStatus.textContent += " — site added!";
+  }
+  else if (0 === NQueueAvailable && RemoveSite(DomMain, ElemContainer)) {
+    ElemStatus.textContent += " — site removed!";
+  }
+  else if (!BDomInL) {
+    //console.log("Site list: '" + LDomSites + "'");
+    ElemContainer.removeChild(ElemStatus);
+    BPaused = true;
   }
 }
 
+var LHrefToOpen = [];
+if (BInQueue) {
+  TitleBase = document.title.replace(/^Review /, "");
+  TmrQueueStatus = setInterval(CheckQueueStatus, 0.25 * 1000);
+}
+else {
+  SetFavicon(GM_getResourceURL("icon"));
+  LHrefToOpen = GetLHrefToOpen();
+  var ElemHeader = document.querySelector(".subheader.tools-rev"), ElemMetaLoadProgress;
+  if (!BChildMeta) {
+    ElemMetaLoadProgress = CreateElemMetaLoadProgress();
+    ElemHeader.appendChild(ElemMetaLoadProgress);
+    CheckSiteMembership(NlNumAvailable.length, ElemHeader, ElemMetaLoadProgress);
+  }
+}
+
+function AddPauseButton(ElemContainer, ElemMarker) {
+  var ElemPause = document.createElement("a");
+  ElemPause.href = "#";
+  ElemPause.style.cssFloat = "right";
+  ElemPause.style.marginTop = "1em";
+  ElemPause.style.marginLeft = "1em";
+  ElemPause.textContent = "Pause";
+  
+  ElemPause.addEventListener("click", function (e) {
+      ElemPause.textContent = BPaused ? "Pause" : "Resume"
+      BPaused = !BPaused;
+      if (e) e.preventDefault();
+      return false;
+    });
+  
+  ElemContainer.insertBefore(ElemPause, ElemMarker);
+}
 //console.log("MS: %d; Last: %d; %d | %d", (new Date()).valueOf(), MstLastMetaLoad, MstLastMetaLoad + MsiMetaLoad, (new Date()).valueOf() + MsiReload);
 if (LHrefToOpen.length > 0) {
   for (let i = BChildMeta ? 0 : 1; i < LHrefToOpen.length; i++) {
@@ -193,31 +234,15 @@ if (LHrefToOpen.length > 0) {
 else if (!BChildMeta) {
   //console.log("Setting reload timeout");
   if (!BInQueue) {
-    var ElemPause = document.createElement("a");
-    ElemPause.href = "#";
-    ElemPause.style.cssFloat = "right";
-    ElemPause.style.marginTop = "1em";
-    ElemPause.style.marginLeft = "1em";
-    ElemPause.textContent = "Pause";
-    if (ElemMetaLoadProgress) {
-      ElemHeader.insertBefore(ElemPause, ElemMetaLoadProgress);
-    } else {
-      ElemHeader.appendChild(ElemPause);
-    }
-    ElemPause.addEventListener("click", function (e) {
-        ElemPause.textContent = BPaused ? "Pause" : "Resume"
-        BPaused = !BPaused;
-        if (e) e.preventDefault();
-        return false;
-      });
+    AddPauseButton(ElemHeader, ElemMetaLoadProgress);
   }
   
-  function LoadNext() {
+  function TryLoadNext() {
     if (!BPaused) {
       CheckNextPage();
     }
   }
-  setInterval(LoadNext, BInQueue ? MsiReloadInQueue : MsiReload);
+  setInterval(TryLoadNext, BInQueue ? MsiReloadInQueue : MsiReload);
 }
 if (BChildMeta && !BInQueue) {
   //console.log("Loading main");
