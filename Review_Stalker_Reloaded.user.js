6dfdba2fd5d56b4082e28a7f94dfdb7947263762
Nathan Tuggy <nathan@tuggycomputer.com>
2021-03-04 21:20:12 -0600

    Adding licensing.

--- Review_Stalker_Reloaded.user.js
+++ Review_Stalker_Reloaded.user.js
@@ -24,6 +24,22 @@
 // @resource    CSSConfig config.css
 // @require     https://openuserjs.org/src/libs/sizzle/GM_config.js
 // ==/UserScript==
+
+/* Copyright Nathan Tuggy 2016-2019
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const HrefBlankFavicon = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
 const LDomNoChildMeta = ["stackapps.net", "meta.stackexchange.com"];
 const NamTempBase = "__RSR_TEMP__";
